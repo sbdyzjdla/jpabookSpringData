feaac2563f402f25edc27b9b6fba695f10e54cc1
yoonsung <sbdyzjdla@gmail.com>
2021-05-26 22:45:49 +0900

    Initial commit

---
+++ src/test/java/jpabook/jpabook/MemberRepositoryTest.java
@@ -0,0 +1,39 @@
+package jpabook.jpabook;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MemberRepositoryTest {
+    //junit5 사용
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember() {
+        Member member = new Member();
+        member.setUsername("memberA");
+        Long saveId = memberRepository.save(member);
+
+        Member findMember = memberRepository.find(saveId);
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
+
+    }
+}
