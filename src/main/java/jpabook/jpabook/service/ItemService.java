04cf778cac91f1c2556f11ffd182de1a8dd08181
yoonsung <sbdyzjdla@gmail.com>
2021-05-31 23:26:06 +0900

    상품기능

---
+++ src/main/java/jpabook/jpabook/service/ItemService.java
@@ -0,0 +1,29 @@
+package jpabook.jpabook.service;
+
+import jpabook.jpabook.domain.Item;
+import jpabook.jpabook.repository.ItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class ItemService {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItem() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
