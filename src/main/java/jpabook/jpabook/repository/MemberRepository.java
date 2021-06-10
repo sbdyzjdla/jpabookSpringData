ce821ed439844bd6f465cf5b8d2f2d91cfbc5ee2
yoonsung <sbdyzjdla@gmail.com>
2021-06-11 01:37:00 +0900

    jpa -> spring data jpa

--- src/main/java/jpabook/jpabook/repository/MemberRepository.java
+++ src/main/java/jpabook/jpabook/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package jpabook.jpabook.repository;
 
 import jpabook.jpabook.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -8,27 +9,7 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
-public class MemberRepository {
+public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    @PersistenceContext
-    EntityManager em;
-
-    public void save(Member member) {
-        em.persist(member);
-    }
-
-    public Member findOne(Long id) {
-        return em.find(Member.class, id);
-    }
-
-    public List<Member> findAll() {
-        return em.createQuery("select m from Member m", Member.class).getResultList();
-    }
-
-    public List<Member> findByName(String name) {
-        return em.createQuery("select m from Member m where m.name = :name", Member.class)
-                .setParameter("name", name)
-                .getResultList();
-    }
-    //sss
+    List<Member> findByName(String name);
 }
--- src/main/java/jpabook/jpabook/repository/OrderRepository.java
+++ src/main/java/jpabook/jpabook/repository/OrderRepository.java
@@ -2,6 +2,8 @@ package jpabook.jpabook.repository;
 
 import jpabook.jpabook.domain.Order;
 import jpabook.jpabook.domain.OrderSearch;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
@@ -16,56 +18,50 @@ import java.util.List;
 import java.util.function.Predicate;
 
 @Repository
-public class OrderRepository {
+public interface OrderRepository extends JpaRepository<Order, Long>, JpaSpecificationExecutor<Order> {
 
-    @PersistenceContext
-    EntityManager em;
+    //jpa 명세(specification)를 이용한 검색 기능 구현
 
-    public void save(Order order) {
-        em.persist(order);
-    }
 
-    public Order findOne(Long id) {
-        return em.find(Order.class, id);
-    }
 
-    public List<Order> findAll (OrderSearch orderSearch) {
-        //JPQL
-        String jpql = "select o from Order o join o.member m";
-        boolean isFirstCondition = true;
-
-        //주문 상태 검색
-        if(orderSearch.getOrderStatus() != null) {
-            if(isFirstCondition) {
-                jpql += " where";
-                isFirstCondition = false;
-            } else {
-                jpql += " and";
-            }
-            jpql += " o.status = :status";
-        }
-
-        //회원 이름 검색
-        if(StringUtils.hasText(orderSearch.getMemberName())) {
-            if(isFirstCondition) {
-                jpql += " where";
-            } else {
-                jpql +=  " and";
-            }
-            jpql += " m.name like :name";
-        }
-
-        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
-                .setMaxResults(1000);  //최대 1000건
-
-        if(orderSearch.getOrderStatus() != null) {
-            query = query.setParameter("status", orderSearch.getOrderStatus());
-        }
-
-        if(StringUtils.hasText(orderSearch.getMemberName())) {
-            query = query.setParameter("name", orderSearch.getMemberName());
-        }
-
-        return query.getResultList();
-    }
+    //예전 검색로직
+//    public List<Order> findAll (OrderSearch orderSearch) {
+//        //JPQL
+//        String jpql = "select o from Order o join o.member m";
+//        boolean isFirstCondition = true;
+//
+//        //주문 상태 검색
+//        if(orderSearch.getOrderStatus() != null) {
+//            if(isFirstCondition) {
+//                jpql += " where";
+//                isFirstCondition = false;
+//            } else {
+//                jpql += " and";
+//            }
+//            jpql += " o.status = :status";
+//        }
+//
+//        //회원 이름 검색
+//        if(StringUtils.hasText(orderSearch.getMemberName())) {
+//            if(isFirstCondition) {
+//                jpql += " where";
+//            } else {
+//                jpql +=  " and";
+//            }
+//            jpql += " m.name like :name";
+//        }
+//
+//        TypedQuery<Order> query = em.createQuery(jpql, Order.class)
+//                .setMaxResults(1000);  //최대 1000건
+//
+//        if(orderSearch.getOrderStatus() != null) {
+//            query = query.setParameter("status", orderSearch.getOrderStatus());
+//        }
+//
+//        if(StringUtils.hasText(orderSearch.getMemberName())) {
+//            query = query.setParameter("name", orderSearch.getMemberName());
+//        }
+//
+//        return query.getResultList();
+//    }
 }
